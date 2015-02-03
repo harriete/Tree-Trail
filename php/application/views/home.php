254b4d33cb77a92693781bb319ecda4b17bcbd0c
Joseph <fskreuz@gmail.com>
2015-02-03 02:28:04 +0000

    Initial commit

---
+++ php/application/views/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title>Tree Trail</title>
+
+  <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap-theme.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('static/node_modules/leaflet/dist/leaflet.css'); ?>">
+
+  <style>
+    html, body, .content {
+      position:relative;
+      width : 100%;
+      height: 100%;
+    }
+  </style>
+
+</head>
+<body>
+
+  <div class="content"></div>
+
+  <script src="<?= base_url('static/node_modules/jquery/dist/jquery.min.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/requirejs/require.js'); ?>"></script>
+  <script src="<?= base_url('static/scripts/home/main.js'); ?>"></script>
+
+</body>
+</html>
---
+++ php/static/scripts/home/main.js
@@ -0,0 +1,20 @@
+require.config({
+  baseUrl: '/static',
+  paths: {
+    ractive : 'node_modules/ractive/ractive',
+    rvc: 'node_modules/rvc/rvc',
+    css: 'node_modlues/require-css/css',
+    leaflet: 'node_modules/leaflet/dist/leaflet',
+  },
+  waitSeconds: 60
+});
+
+require([
+  'rvc!components/tree-app'
+],function(TreeApp){
+
+  new TreeApp({
+    el: '.content'
+  });
+
+});
