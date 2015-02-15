a177eafb247e2b99aaaecf9f8194e5d0df485af5
not-jay <jaysonsona@tfwno.gf>
2015-02-15 22:57:44 +0800

    Initial commit for DB creation and test script

---
+++ php/application/controllers/init_db.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Init_db extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->load->model("init_db_model", "init_db");
+	}
+
+	public function index() {
+		//run checks
+
+
+		$this->load->view("init_db/status", $data);
+	}
+
+	public function start_init() {
+		
+	}
+
+}
---
+++ php/application/models/init_db_model.php
@@ -0,0 +1,55 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Init_db_model extends CI_Model {
+
+	function __construct() {
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->dbforge();
+		$this->load->dbutil();
+
+		$this->db_name = getenv("OPENSHIFT_MYSQL_DB_NAME");
+		$this->db_tables = array(
+			"comments", "locations",
+			"photos", "users"
+		);
+	}
+
+	function check_if_tables_exist() {
+		$tables = $this->db->list_tables();
+		$diff = array_filter(array_diff($this->db_tables, $tables));
+
+		return empty($diff);
+	}
+
+	function check_for_partial_tables() {
+		$tables = $this->db->list_tables();
+		$diff = array_filter(array_diff($this->db_tables, $tables));
+
+		return !$this->check_if_tables_exist() &&
+			   count($diff) != count($this->db_tables);
+	}
+
+	function modify_db() {
+		// $this->dbforge->create_database("tree_trail");
+		$query = "ALTER DATABASE `".$this->db_name."`
+				  CHARACTER SET utf8
+				  COLLATE utf8_general_ci";
+
+		return $this->db->query($query);
+	}
+
+	function drop_tables() {
+		$tables = $this->db->list_tables();
+
+		foreach($tables as $table):
+			$this->dbforge->drop_table($table);
+		endforeach;
+	}
+
+	function create_tables() {
+		
+	}
+
+}
