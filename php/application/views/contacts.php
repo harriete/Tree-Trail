4da2e698ff57a51e455ba71d439908d73f3f3403
arpelayo <afp.1992@gmail.com>
2015-02-16 08:51:10 -0800

    Added contact page with dummy value.

---
+++ php/application/views/contacts.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title>Tree Trail</title>
+
+  <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap-theme.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('static/node_modules/leaflet/dist/leaflet.css'); ?>">
+
+  <style>
+    html, body, .content {
+      position:relative;
+      width : 100%;
+      height: 100%;
+    }
+
+    h1{
+      text-align: center;
+    }
+  </style>
+
+</head>
+<body>
+  
+
+  <div class="content"> 
+    <div class=""> <h1>Contacts</h1>  </div> 
+
+    <div class="col-lg-3"></div>
+    <div class="col-lg-6">
+   <table class="table table-striped">
+      <tr>
+
+        <th>Contact Person</th>
+        <th>Contact Number</th>
+        <th>Connections</th>
+      </tr>
+      <tr>
+        <td>John Doe</td>
+        <td>093XXXXXXXX</td>
+        <td>World Tree Productions</td>
+      </tr>
+    </table></div>
+    <div class="col-lg-3"></div>
+  </div>
+
+  <script src="<?= base_url('static/node_modules/jquery/dist/jquery.min.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/requirejs/require.js'); ?>"></script>
+  <!--<script src="<?= base_url('static/scripts/home/main.js'); ?>"></script>-->
+
+</body>
+</html>
