b92f63152cd122c16d8b645e307261963914ba05
not-jay <jaysonsona@tfwno.gf>
2015-02-16 11:29:11 +0800

    Added initial test suite

--- php/application/controllers/init_db.php
+++ php/application/controllers/init_db.php
@@ -10,8 +10,13 @@ class Init_db extends CI_Controller {
 
 	public function index() {
 		//run checks
+		$this->db_checker->reset_results();
 
+		$this->db_checker->check_if_tables_exist();
+		$this->db_checker->check_for_partial_tables();
+		$this->db_checker->check_db_table_collations();
 
+		$data = $this->db_checker->results();
 		$this->load->view("init_db/status", $data);
 	}
 
