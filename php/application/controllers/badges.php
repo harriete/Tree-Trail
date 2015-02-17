93cdecfe840211b1b5b0ce05726d7c6146532533
Joseph <fskreuz@gmail.com>
2015-02-17 16:41:22 +0000

    Added badge models and controllers

---
+++ php/application/controllers/badges.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Badges extends REST_Controller {
+
+  public function index_get(){
+    $this->response(['test']);
+  }
+
+  public function index_post(){
+
+  }
+
+  public function index_put(){
+
+  }
+
+  public function index_delete(){
+
+  }
+
+}
---
+++ php/application/models/badges_model.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Badges_model extends CI_Model {
+
+
+  public function create($data = []){
+
+  }
+
+  public function read($id = null){
+    return ['rar'];
+
+  }
+
+  public function update($data = []){
+
+  }
+
+  public function delete($id = null){
+
+  }
+
+}
