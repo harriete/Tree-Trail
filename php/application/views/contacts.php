b5f706bace1b9c141c662bcbb2d9238133999ac2
Joseph <fskreuz@gmail.com>
2015-02-17 14:10:15 +0000

    Removing unneeded code in contacts page

--- php/application/views/contacts.php
+++ php/application/views/contacts.php
@@ -7,19 +7,6 @@
 
   <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap.min.css'); ?>">
   <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap-theme.min.css'); ?>">
-  <link rel="stylesheet" href="<?= base_url('static/node_modules/leaflet/dist/leaflet.css'); ?>">
-
-  <style>
-    html, body, .content {
-      position:relative;
-      width : 100%;
-      height: 100%;
-    }
-
-    h1{
-      text-align: center;
-    }
-  </style>
 
 </head>
 <body>
@@ -48,8 +35,6 @@
 
   <script src="<?= base_url('static/node_modules/jquery/dist/jquery.min.js'); ?>"></script>
   <script src="<?= base_url('static/node_modules/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
-  <script src="<?= base_url('static/node_modules/requirejs/require.js'); ?>"></script>
-  <!--<script src="<?= base_url('static/scripts/home/main.js'); ?>"></script>-->
 
 </body>
 </html>
