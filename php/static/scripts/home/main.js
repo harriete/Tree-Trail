dd1fffaf387f1e9af2c8b05ed18a4a15b15252c7
Joseph <fskreuz@gmail.com>
2015-02-22 16:14:07 +0000

    Added simple validation client-side

--- php/static/scripts/home/main.js
+++ php/static/scripts/home/main.js
@@ -4,8 +4,9 @@ require.config({
     ractive : 'node_modules/ractive/ractive',
     rvc: 'node_modules/rvc/rvc',
     leaflet: 'node_modules/leaflet/dist/leaflet',
+    validate: 'node_modules/validate.js/validate',
   },
-  waitSeconds: 60
+  waitSeconds: 60,
 });
 
 require([
