2ed44ea77d0cc04feaa09d09839d52c093325990
Joseph <fskreuz@gmail.com>
2015-03-12 14:15:04 +0000

    Updating subclass prefix for better classification

--- php/application/models/badges_model.php
+++ php/application/models/badges_model.php
@@ -1,31 +1,8 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Badges_Model extends CI_Model {
+class Badges_model extends TreeTrailModel {
 
   public $tableName = 'locations';
   public $tableId = 'id';
-
-  public function create($data = []){
-    $success = $this->db->insert($this->tableName, $data);
-    return ($success) ? $this->retrieveLastSaved($this->db->insert_id()) : $success;
-  }
-
-  public function read(){
-    return $this->db->get($this->tableName)->result_array();
-  }
-
-  public function update($data = []){
-    $success = $this->db->where($this->tableId, $data[$this->tableId])->update($this->tableName, $data);
-    return ($success) ? $this->retrieveLastSaved($data[$this->tableId]) : $success;
-  }
-
-  public function delete($data = []){
-    return $this->db->where($this->tableId, $data[$this->tableId])->delete($this->tableName);
-  }
-
-  private function retrieveLastSaved($id){
-    $this->db->flush_cache();
-    return $this->db->where($this->tableId, $id)->get($this->tableName)->row_array();
-  }
 
 }
