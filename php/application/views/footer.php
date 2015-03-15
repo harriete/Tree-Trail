55df035081907e466d9e4ce39a33d05459ac5e6e
Joseph <fskreuz@gmail.com>
2015-03-15 08:15:56 +0000

    Moving dashboard deps to npm

--- php/application/views/footer.php
+++ php/application/views/footer.php
@@ -1,10 +1,10 @@
   
   <script src="<?= base_url('static/node_modules/jquery/dist/jquery.js'); ?>"></script>
-  <script src="<?= base_url('static/node_modules/bootstrap/dist/js/bootstrap.js'); ?>"></script><script src="<?= base_url('static/node_modules/jquery/dist/jquery.dataTables.min.js'); ?>"></script>
-  <script src="<?= base_url('static/node_modules/jquery/dist/jquery.cycle2.min.js'); ?>"></script>
-  <script src="<?= base_url('static/node_modules/jquery/dist/jquery.cycle2.scrollVert.reversed.js'); ?>"></script>
-  <script src="<?= base_url('static/node_modules/jquery/dist/moment.js'); ?>"></script>
-  <script src="<?= base_url('static/node_modules/jquery/dist/datatables.responsive.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/bootstrap/dist/js/bootstrap.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/moment/min/moment.min.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/jquery-cycle2/build/jquery.cycle2.min.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/jquery-cycle2/build/plugin//jquery.cycle2.scrollVert.min.js'); ?>"></script>
+  <script src="<?= base_url('static/node_modules/datatables/media/js/jquery.dataTables.min.js'); ?>"></script>
 
 </body>
 </html>
--- php/application/views/header.php
+++ php/application/views/header.php
@@ -7,7 +7,7 @@
 
   <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap.min.css'); ?>">
   <link rel="stylesheet" href="<?= base_url('static/node_modules/bootstrap/dist/css/bootstrap-theme.min.css'); ?>">
-  <link rel="stylesheet" href="<?= base_url('static/node_modules/leaflet/dist/leaflet.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('static/node_modules/datatables/media/css/jquery.dataTables.min.css'); ?>">
 
   <link rel="stylesheet" href="<?= base_url('static/css/manage_users.css'); ?>">
   <style>
